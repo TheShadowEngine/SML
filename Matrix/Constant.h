b644d6e216644625dbfeb7c8e7920013fce63c11
krishpranav <krisna.pranav@gmail.com>
2023-06-21 20:10:45 +0530

    Matrix: constant

---
+++ Matrix/Constant.h
@@ -0,0 +1,14 @@
+//
+//  Constant.h
+//  SML
+//
+//  Created by krisna pranav on 21/06/23.
+//
+
+#ifndef Constant_h
+#define Constant_h
+
+
+#define ARC4RANDOM_MAX 0x100000000
+
+#endif /* Constant_h */
