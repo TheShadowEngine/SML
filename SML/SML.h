4899793ce47a7605e28f1fbf74f2d31d87f2eb22
krishpranav <krisna.pranav@gmail.com>
2023-06-21 20:09:06 +0530

    add files via upload

---
+++ SML/SML.h
@@ -0,0 +1,18 @@
+//
+//  SML.h
+//  SML
+//
+//  Created by krisna pranav on 21/06/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SML.
+FOUNDATION_EXPORT double SMLVersionNumber;
+
+//! Project version string for SML.
+FOUNDATION_EXPORT const unsigned char SMLVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SML/PublicHeader.h>
+
+
