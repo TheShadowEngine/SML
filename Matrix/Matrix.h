2b7ad07b05a217166f12282436358f05ce557f57
krishpranav <krisna.pranav@gmail.com>
2023-06-21 20:21:12 +0530

    Matrix: Matrix

--- Matrix/Matrix.h
+++ Matrix/Matrix.h
@@ -27,3 +27,24 @@ typedef enum refMode {
 +(instancetype)onesLike:(Matrix *)other;
 
 +(instancetype)matrixOfRows:(int)m columns:(int)n value:(double)val;
+
++(instancetype)matrixOfRows:(int)m columns:(int)n valueInDiagonal:(double)diagonal value:(double)val;
+
++(instancetype)matrixOfRows:(int)m columns:(int)m valuesInDiagonal:(double *)diagonal value:(double)val;
+
++(instancetype)matrixFromArray:(double *)arr rows:(int)m columns:(int)n;
+
++(instancetype)matrixFromArray:(double *)arr rows:(int)m columns:(int)n mode:(refMode)mode;
+
++(instancetype)matrixFromNSArray:(NSArray *)arr rows:(int)m columns:(int)n;
+
++(instancetype)matrixFromMatrix:(Matrix *)other;
+
++(instancetype)identityOfRows:(int)m columns:(int)n;
+
+-(double)valueAtRow:(int)row column:(int) column;
+
+-(double)i:(int)i j:(int)j;
+
+-(void)setValue:(double)value row:(int)row column:(int)column;
+
